916d99f3ff6864f79425ef56b081d33f4503058b
StevenYAMBOS <stevenyambos@gmail.com>
2026-01-24 22:49:20 +0100

    fix : MAJ Cargo
    Mise à jour de la version de Rust.
    Solution : https://forum.exercism.org/t/cli-not-working-with-2024-edition/15991
    En gros j'ai fais la commande `rustup update stable` pour mettre à jour Rust.

--- BACK_END/src/database.rs
+++ BACK_END/src/database.rs
@@ -7,9 +7,13 @@ use std::env;
 async fn database() -> PgPool {
     dotenv()?;
     let conn_string = env::var("DATABASE_URL")?;
-    let builder = SslConnector::builder(SslMethod::tls())?;
-    let connector = MakeTlsConnector::new(builder.build());
-    let mut pool = Pool::connect(&conn_string, connector)?;
-    println!("Connection established");
-    Ok(())
+    let pool = sqlx::postgres::PgPool::connect(conn_string).await.unwrap();
+
+    pool
+    // let conn_string = env::var("DATABASE_URL")?;
+    // let builder = SslConnector::builder(SslMethod::tls())?;
+    // let connector = MakeTlsConnector::new(builder.build());
+    // let mut pool = Pool::connect(&conn_string, connector)?;
+    // println!("Connection established");
+    // Ok(())
 }
