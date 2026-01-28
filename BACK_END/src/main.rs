90c4154bd72221761f7969c48cbedb49e26059f0
StevenYAMBOS <stevenyambos@gmail.com>
2026-01-28 23:28:15 +0100

    feat : Connexion à la base de données fonctionnelle
    J'utilise SQLX avec une base de données PSQL (pour le moment en local, plus tard sûrement sur Digital Ocean)

--- BACK_END/src/main.rs
+++ BACK_END/src/main.rs
@@ -1,10 +1,22 @@
-use axum::{routing::get, Router};
+use axum::{Router, routing::get};
+use dotenv::dotenv;
+use sqlx::postgres::PgPoolOptions;
+use std::env;
 
 #[tokio::main]
 async fn main() {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&database_url)
+        .await
+        .expect("Failed to create pool.");
+    println!("Connection established");
+
     let app = Router::new().route("/", get(|| async { "Hello, World !" }));
 
-    database();
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
