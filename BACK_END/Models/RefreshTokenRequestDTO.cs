9b1169652e35ba197a04ec6e8baa328c545fd795
StevenYAMBOS <stevenyambos@gmail.com>
2026-02-04 15:45:59 +0100

    feat : Modèles refresh token

---
+++ BACK_END/Models/RefreshTokenRequestDTO.cs
@@ -0,0 +1,8 @@
+namespace Portfolio.Models
+{
+  public class RefreshTokenRequestDTO
+  {
+    public Guid UserId { get; set; }
+    public required string RefreshToken { get; set; }
+  }
+}
---
+++ BACK_END/Models/TokenResponseDTO.cs
@@ -0,0 +1,8 @@
+namespace Portfolio.Models
+{
+  public class TokenResponseDTO
+  {
+    public required string AccessToken { get; set; }
+    public required string RereshToken { get; set; }
+  }
+}
