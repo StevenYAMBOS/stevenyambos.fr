1ae5660dd1247de2e34239a4e132f84695ac683e
StevenYAMBOS <stevenyambos@gmail.com>
2026-02-08 11:37:27 +0100

    feat : Implémentation Serilog

--- BACK_END/Program.cs
+++ BACK_END/Program.cs
@@ -4,44 +4,63 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Portfolio.Data;
 using Portfolio.Services;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
 
-builder.Services.AddControllers();
-
-var jwtSecret = builder.Configuration["AppSettings:Token"];
-var key = Encoding.ASCII.GetBytes(jwtSecret);
-var issuer = builder.Configuration["AppSettings:Issuer"];
-var audience = builder.Configuration["AppSettings:Audience"];
-
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+try
 {
-  options.TokenValidationParameters = new TokenValidationParameters
+  Log.Information("INFO : Lancement de l'application");
+  var builder = WebApplication.CreateBuilder(args);
+  builder.Host.UseSerilog((context, loggerConfig) =>
+      loggerConfig.ReadFrom.Configuration(context.Configuration)
+  );
+
+  builder.Services.AddControllers();
+
+  var jwtSecret = builder.Configuration["AppSettings:Token"];
+  var key = Encoding.ASCII.GetBytes(jwtSecret);
+  var issuer = builder.Configuration["AppSettings:Issuer"];
+  var audience = builder.Configuration["AppSettings:Audience"];
+
+  builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
   {
-    ValidateIssuer = true,
-    ValidIssuer = issuer,
-    ValidateAudience = true,
-    ValidAudience = audience,
-    ValidateIssuerSigningKey = true,
-    IssuerSigningKey = new SymmetricSecurityKey(key)
-  };
-});
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+      ValidateIssuer = true,
+      ValidIssuer = issuer,
+      ValidateAudience = true,
+      ValidAudience = audience,
+      ValidateIssuerSigningKey = true,
+      IssuerSigningKey = new SymmetricSecurityKey(key)
+    };
+  });
 
-builder.Services.AddScoped<IAuthService, AuthService>();
+  builder.Services.AddScoped<IAuthService, AuthService>();
 
-// Configurer DbContext avec le connexion Psql
-builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+  // Configurer DbContext avec le connexion Psql
+  builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-var app = builder.Build();
+  var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-  app.Logger.LogInformation("Lancement de l'app !");
-  app.UseDeveloperExceptionPage();
+  if (app.Environment.IsDevelopment())
+  {
+    app.UseDeveloperExceptionPage();
+  }
+
+  app.UseHttpsRedirection();
+  app.UseAuthentication();
+  app.MapControllers();
+
+  app.Run();
 }
-
-app.UseHttpsRedirection();
-app.UseAuthentication();
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+  Log.Fatal(ex, "ERREUR : Problème lors du lancement de l'application");
+}
+finally
+{
+  Log.CloseAndFlush();
+}
