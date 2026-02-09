4637b94e9c1dc5f2e214a2ed56a2aabdf489941a
StevenYAMBOS <stevenyambos@gmail.com>
2026-02-09 21:53:35 +0100

    feat : Méthodes articles

---
+++ BACK_END/Services/IArticleService.cs
@@ -0,0 +1,11 @@
+using Portfolio.Entities;
+using Portfolio.Models;
+
+namespace Portfolio.Articles
+{
+  public interface IArticles
+  {
+    Task<Article> CreateArticleAsync(ArticleDTO request);
+    Task<Article> GetOneArticleAsync(ArticleDTO request);
+  }
+}
