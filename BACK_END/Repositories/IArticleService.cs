534a32e6ff83d30b0b33dae3867db601efb0e60a
StevenYAMBOS <stevenyambos@gmail.com>
2026-02-12 19:19:44 +0100

    feat : Repository articles

--- BACK_END/Repositories/IArticleService.cs
+++ BACK_END/Repositories/IArticleService.cs
@@ -5,7 +5,9 @@ namespace Portfolio.Repositories
 {
     public interface IArticles
     {
-        Task<Article?> CreateArticleAsync(ArticleDTO request);
-        Task<Article?> GetOneArticleAsync(ArticleDTO request);
+        Task<Article> CreateArticleAsync(ArticleDTO request);
+        Task<IEnumerable<Article>> GetArticlesAsync();
+        Task<Article?> FindArticleByIdAsync(Guid id);
+        Task DeleteArticleAsync(ArticleDTO request);
     }
 }
