f68e30cc10b29deb0112e1ece2f3672cdd8bc31e
StevenYAMBOS <stevenyambos@gmail.com>
2026-03-18 10:55:38 +0100

    wip : Email contact file attachment en cours

--- Services/EmailService.cs
+++ Services/EmailService.cs
@@ -8,7 +8,7 @@ namespace Portfolio.Services;
 
 public class EmailService(ILogger<EmailService> logger) : IEmailService
 {
-    public async Task<string> SendEmail(string toEmail, string subject, string body)
+    public async Task<string> SendEmail(string toEmail, string subject, string body, string path)
     {
         try
         {
@@ -18,10 +18,41 @@ public class EmailService(ILogger<EmailService> logger) : IEmailService
             var password = Environment.GetEnvironmentVariable("smtpPassword");
 
             var email = new MimeMessage();
-            email.From.Add(new MailboxAddress("Your Name", from));
-            email.To.Add(new MailboxAddress("To Name", toEmail));
+            email.From.Add(new MailboxAddress("stevenyambos.fr", from));
+            email.To.Add(new MailboxAddress("Me", toEmail));
             email.Subject = subject;
-            email.Body = new TextPart("html") { Text = body };
+            // email.Body = new TextPart("html") { Text = body };
+
+            var builder = new BodyBuilder
+            {
+                // Set the plain-text version of the message text
+                TextBody = body
+            };
+
+            // We may also want to attach a calendar event for Monica's party...
+            builder.Attachments.Add(path);
+
+            // Now we just need to set the message body and we're done
+            email.Body = builder.ToMessageBody();
+
+            /*             var attachment = new MimePart("image", "gif")
+                        {
+                            Content = new MimeContent(File.OpenRead(path)),
+                            ContentDisposition = new ContentDisposition(ContentDisposition.Attachment),
+                            ContentTransferEncoding = ContentEncoding.Base64,
+                            FileName = Path.GetFileName(path)
+                        };
+
+                        // now create the multipart/mixed container to hold the message text and the
+                        // image attachment
+                        var multipart = new Multipart("mixed")
+                        {
+                            body,
+                            attachment
+                        };
+
+                        // now set the multipart/mixed as the message body
+                        email.Body = multipart; */
 
             logger.LogInformation("Destinataire : {0}", from);
             logger.LogInformation("Expéditeur : {0}", toEmail);
